a44f47a6e0ad6639cc4230086b6e519d40ac8f1e
s3775959 <62049586+s3775959@users.noreply.github.com>
2021-07-21 15:25:47 +0700

    file for test cases

---
+++ src/SomeMethodsTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SomeMethodsTest {
+
+	@Test
+	void testWeight_index() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGet_weight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGet_height() {
+		fail("Not yet implemented");
+	}
+
+}
